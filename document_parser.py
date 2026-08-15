35a1f7eef327412e3c846b06ad059414918de1cb
Prashant Singh <prshnnt@gmail.com>
2026-08-15 18:17:37 +0530

    chore: added function document_to_lists() to get page_content and metadata as list for ingestion

--- document_parser.py
+++ document_parser.py
@@ -1,6 +1,7 @@
+from typing import List
 import opendataloader_pdf as odlp
 from langchain_opendataloader_pdf import OpenDataLoaderPDFLoader
-from typing import List
+from langchain_core.documents import Document
 
 class Convert:
     @staticmethod
@@ -14,11 +15,20 @@ class Convert:
         """
         odlp.convert(input_path=path,output_dir=output_dir,format="markdown")
     @staticmethod
-    def langchain_load_pdf(path:str|List[str],format="text"):
+    def langchain_load_pdf(path:str|List[str],format="text",extra_metadata:dict = {}):
         loader = OpenDataLoaderPDFLoader(
             file_path=path,
             format=format,
             quiet=True,
+            extra_metadata=extra_metadata
         )
         documents = loader.load()
         return documents
+
+def document_to_lists(docs:List[Document]):
+    content = []
+    metadata = []
+    for doc in docs:
+        content.append(doc.page_content)
+        metadata.append(doc.metadata)
+    return [content,metadata]
